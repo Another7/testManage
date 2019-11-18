fe8a2e9795db8173d69e55bf19a19b457d14c82e
zhang009 <42031101+zhang009@users.noreply.github.com>
2019-11-18 17:21:09 +0800

    创建导出试卷controller方法

--- src/com/test/controller/TestPaperController.java
+++ src/com/test/controller/TestPaperController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -118,6 +119,13 @@ public class TestPaperController {
 		return "TestPaperView";// 转发到TestPaperView
 	}
 	
-	
+	//根据试卷id导出试卷
+	@RequestMapping(value = "/exportTestPaper.action", method = RequestMethod.GET)
+	@ResponseBody
+	public void exportTestPaper(HttpServletRequest request,HttpServletResponse response,String tpId) {
+				
+		
+		
+	}
 
 }
